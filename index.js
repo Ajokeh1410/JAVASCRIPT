e6ececf63f16e693c873871c25f7f49bbac15b89
Ajokeh1410 <kaosarahkazeem1@gmail.com>
2024-01-12 16:57:23 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+console.log ("Hello world!")
+let name=("Kawthar");
+console.log(name);
+name="Ajokeh";
+console.log(name)
+let age = 57;
+let newAge = 13;
+let percentage =newAge/age;
+let myString = "hello";
+let otherString = "hi";
+let backString = "hi again";
+let num = 33;
+let bol = true;
+let undefinedVariable;
+let emptyValue = null;
+console.log(myString);
+console.log(otherString);
+console.log(backString);
+console.log(num);
+console.log(bol);
+console.log(undefinedVariable);
+console.log(emptyValue);
+//console.log(percentage);
+console.log(typeof myString);
+console.log(typeof otherString);
+console.log(typeof backString);
+
+var test ="testing"
+console.log(test)
+var test = "hello"
+console.log(test)
+
+let myName = "Quasim kawthar";
+myName = "quasim kawthar"
+console.log(myName)
+
